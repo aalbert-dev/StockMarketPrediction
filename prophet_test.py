35e6b50245cd93b49a1ca413177f3ab751ed9572
aalbert-dev <arjunalbert1@gmail.com>
2021-04-25 12:56:58 -0400

    Sucsessful prophet test

--- prophet_test.py
+++ prophet_test.py
@@ -1,2 +1,18 @@
 import pandas as pd
 from prophet import Prophet
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("/home/arjun/Desktop/Programming Assignments/StockMarketPrediction/data/TRI.csv")
+df = df.rename(columns={"Date": "ds", "Close": "y"})
+print(df.head())
+
+m = Prophet()
+m.fit(df)
+
+future = m.make_future_dataframe(periods=365)
+print(future.tail())
+
+forecast = m.predict(future)
+print(forecast.columns)
+print(forecast.tail())
+# forecast[['ds', 'y']].tail()
